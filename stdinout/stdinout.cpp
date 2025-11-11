de616f98d1ce79adef9173654bcf36f2ffa85233
4vis <siva.ezhuthachan@gmail.com>
2025-11-11 23:11:02 +0800

    learnt input and output and how each of them work and how to implement multiple entries etc

---
+++ stdinout/stdinout.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){ 
+    int x{},y{},z{};
+    // std::cin >> x;
+    // std::cout << "you entered: " << x << '\n';
+    // std::cout << "Enter a Number: ";
+    // std::cin >> x;
+    // std::cout << "you entered: " << x << '\n';
+    std::cout << "Enter three numbers: ";
+    std::cin >> x >> y >> z;
+    std::cout << "You've entered: " << x << ',' << y << " and " << z << ".\n";
+}
